717656ee80034be38873ed9793035347f7ff9c09
satyr-software <satyr-software@users.noreply.github.com>
2016-03-10 17:26:02 +1100

    ereg() fix
    Fixed deprecated ereg() for PHP7

--- lang/index.php
+++ lang/index.php
@@ -9,19 +9,19 @@ function parseHttpAcceptLanguage($str=NULL) {
 	foreach ($langs as $lang) { 
 	// parsing language preference instructions 
 	// 2_digit_code[-longer_code][;q=coefficient] 
-	ereg('([a-z]{1,2})(-([a-z0-9]+))?(;q=([0-9\.]+))?',$lang,$found); 
-	// 2 digit lang code 
-	$code=$found[1]; 
-	// lang code complement 
-	$morecode=$found[3]; 
-	// full lang code 
-	$fullcode=$morecode?$code.'-'.$morecode:$code; 
-	// coefficient 
-	$coef=sprintf('%3.1f',$found[5]?$found[5]:'1'); 
-	// for sorting by coefficient 
-	$key=$coef.'-'.$code; 
-	// adding 
-	$accepted[$key]=array('code'=>$code,'coef'=>$coef,'morecode'=>$morecode,'fullcode'=>$fullcode); } 
+	preg_match('/([a-z]{1,2})(-([a-z0-9]+))?(;q=([0-9\.]+))?/',$lang,$found);
+	// 2 digit lang code
+	$code=$found[1];
+	// lang code complement
+	if (isset($found[3])) { $morecode=$found[3];  }
+	// full lang code
+	$fullcode=(isset($morecode)?$code.'-'.$morecode:$code);
+	// coefficient
+	$coef=sprintf('%3.1f',(isset($found[5])?$found[5]:'1'));
+	// for sorting by coefficient
+	$key=$coef.'-'.$code;
+	// adding
+	$accepted[$key]=array('code'=>$code,'coef'=>$coef,'morecode'=>(isset($morecode)?$morecode:""),'fullcode'=>$fullcode); }
 	// sorting the list by coefficient desc 
 	krsort($accepted); 
 	return $accepted; 
